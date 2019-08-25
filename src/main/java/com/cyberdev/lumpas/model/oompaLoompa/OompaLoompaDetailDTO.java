746d49d7ad1c3a311e72300bc4bd527ba1dbc7c8
Marc <denzelnihlus@gmail.com>
2019-08-25 02:03:24 +0200

    validation

--- src/main/java/com/cyberdev/lumpas/model/oompaLoompa/OompaLoompaDetailDTO.java
+++ src/main/java/com/cyberdev/lumpas/model/oompaLoompa/OompaLoompaDetailDTO.java
@@ -4,16 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class OompaLoompaDetailDTO {
 
     private String id;
+    @NotBlank(message = "Name is required")
     private String name;
+    @PositiveOrZero(message = "Please provide a valid age number")
     private Integer age;
+    @NotBlank(message = "Job is required")
     private String job;
+    @NotNull(message = "Height is required")
     private Float height;
+    @NotNull(message = "Weight is required")
     private Float weight;
     private String description;
 
